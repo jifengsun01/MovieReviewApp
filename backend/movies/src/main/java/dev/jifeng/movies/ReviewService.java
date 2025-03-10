81df2a963223ef20df3f7b8d28c277be915cff13
Jifeng Sun <jsun372@gatech.edu>
2025-03-09 23:40:01 -0400

    Review service - change to classic approach with updateFirst

--- backend/movies/src/main/java/dev/jifeng/movies/ReviewService.java
+++ backend/movies/src/main/java/dev/jifeng/movies/ReviewService.java
@@ -3,6 +3,7 @@ package dev.jifeng.movies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Service;
 
@@ -25,10 +26,17 @@ public class ReviewService {
         Review review = new Review(reviewBody, imdbId);
         reviewRepository.insert(review);
         //reviewRepository.insert(review);
-        mongoTemplate.update(Movie.class)
-                .matching(Criteria.where("imdbId").is(imdbId))
-                .apply(new Update().push("reviewIds").value(review))
-                .first();
+        // The Fluent Update API
+//        mongoTemplate.update(Movie.class)
+//                .matching(Criteria.where("imdbId").is(imdbId))
+//                .apply(new Update().push("reviewIds").value(review))
+//                .first();
+
+        // The "Classic" Approach (updateFirst) - easier for testing
+        Query query = new Query(Criteria.where("imdbId").is(imdbId));
+        Update update = new Update().push("reviewIds", review);
+
+        mongoTemplate.updateFirst(query, update, Movie.class);
 
         return review;
     }
